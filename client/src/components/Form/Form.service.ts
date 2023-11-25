6515ff5218812d3a8772f78ad73da3bec02b45a8
Alexandr Tarasiuk <46819896+monstrikwho@users.noreply.github.com>
2023-11-25 19:40:32 +0300

    Поправил стили

--- client/src/components/Form/Form.service.ts
+++ client/src/components/Form/Form.service.ts
@@ -23,32 +23,8 @@ export async function fetchContact({ email, number }: FetchContactProps) {
       }
     );
 
-    const { status, data } = await response.json();
-
-
-    if (status) {
-      return data
-    }
-
-    // if (response.ok) {
-    //   const responseData = await response.json();
-    //   setUsers(responseData);
-    //   reset();
-    // } else {
-    //   console.error("Request failed");
-    //   setError("Request failed");
-    //   setUsers(null);
-    // }
+    return await response.json();
   } catch (error) {
-    // if (error instanceof Error) {
-    //   if (error.name === "AbortError") {
-    //     console.error("Request was aborted");
-    //   } else {
-    //     console.error("A network error occurred", error);
-    //     throw error;
-    //   }
-    // } else {
-    //   console.error("An error occurred", error);
-    // }
+    console.log(error);
   }
 }
